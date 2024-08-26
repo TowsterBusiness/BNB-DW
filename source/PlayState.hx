8fb9d74403a3e361dd9edf225e1ad9b0c1c4402b
Towster_ <79161644+TowsterBusiness@users.noreply.github.com>
2024-08-25 18:04:56 -0700

    game play

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -1,6 +1,12 @@
 package;
 
-import flixel.*;
+import JsonTypes;
+import flixel.FlxBasic;
+import flixel.FlxCamera;
+import flixel.FlxG;
+import flixel.FlxObject;
+import flixel.FlxSprite;
+import flixel.FlxState;
 import flixel.group.FlxGroup.FlxTypedGroup;
 import flixel.group.FlxSpriteGroup.FlxTypedSpriteGroup;
 import flixel.input.keyboard.FlxKey;
@@ -10,7 +16,15 @@ import flixel.tweens.FlxEase;
 import flixel.tweens.FlxTween;
 import flixel.ui.FlxBar;
 import haxe.Timer;
-import towsterFlxUtil.*;
+import towsterFlxUtil.TowPaths;
+import towsterFlxUtil.TowSprite;
+import towsterFlxUtil.TowUtils;
+
+typedef Rank =
+{
+	time:Int,
+	difference:Int
+}
 
 class PlayState extends FlxState
 {
@@ -103,6 +117,7 @@ class PlayState extends FlxState
 		gameoverBlack = new FlxSprite(0, 0).loadGraphic(TowPaths.getFilePath('blackScreen_holed', PNG));
 		gameoverBlack.alpha = 0;
 		add(gameoverBlack);
+
 		// everything after this point will not be covered with black in gameover
 		bob = new TowSprite(675, 190, 'characters/bob_assets');
 		bob.loadAnimations('characters/bob');
@@ -167,123 +182,158 @@ class PlayState extends FlxState
 				{
 					bird.peck();
 				}
-			}
-		}
-	}
+				if (conductor.getMil() > bird.time + bird.actionTime(2))
+				{
+					if (bird.shouldRank)
+					{
+						bird.shouldRank = false;
+						bird.playAnim('squawk');
+						bosip.playAnim('throw MISS');
+						bob.playAnim('grumpy');
+						changeHealth(-20);
+					}
+				}
+				if (conductor.getMil() > bird.time + bird.actionTime(3))
+				{
+					bird.goOut(bird.actionTime(3));
+				}
+			});
 
-	function win()
-	{
-		bob.playAnim('happy');
-		bosip.playAnim('happy');
-		var startFade = new Timer(1000);
-		startFade.run = () ->
-		{
-			openSubState(new SongFinishedSubState(rankList));
-			startFade.stop();
-		}
-	}
-
-	function organizeNotes()
-	{
-		var noteList = songJson.chart;
-
-		var usedTimeList = [];
-
-		birdList.forEachAlive(function(bird)
-		{
-			usedTimeList.push(bird.time);
-		});
-
-		for (note in noteList)
-		{
-			if (!usedTimeList.contains(note.time) && conductor.getMil() < note.time)
+			if (conductor.pastBeat() && !isGameover)
 			{
-				birdList.add(new Bird(note.id, note.time, conductor.getBPM().bpm));
+				bob.playAnim('idle');
+				if (bosip.animation.finished || bosip.animation.curAnim.name == 'idle')
+				{
+					bosip.playAnim('idle');
+				}
+
+				if (healthP1.angle != 5)
+				{
+					FlxTween.tween(healthP1, {angle: 5}, 0.3, {ease: FlxEase.expoOut});
+					FlxTween.tween(healthP2, {angle: -5}, 0.3, {ease: FlxEase.expoOut});
+				}
+				else
+				{
+					FlxTween.tween(healthP1, {angle: -5}, 0.3, {ease: FlxEase.expoOut});
+					FlxTween.tween(healthP2, {angle: 5}, 0.3, {ease: FlxEase.expoOut});
+				}
 			}
 		}
-	}
 
-	function getRank(time:Int)
-	{
-		var difference = Math.abs(conductor.getMil() - time);
+		var preBeats = 4;
 
-		for (index => rank in rankings)
+		function organizeNotes()
 		{
-			if (difference < rank)
-				return index;
-		}
-		return rankings.length;
-	}
+			var noteList = songJson.chart;
 
-	function gameover()
-	{
-		isGameover = true;
-		bob.playAnim('gameover');
-		bosip.playAnim('gameover');
-		conductor.pause();
+			var usedTimeList = [];
 
-		FlxTween.tween(songInst, {volume: 0}, 2);
-		FlxTween.tween(gameoverBlack, {alpha: 0.9}, 2);
-	}
-
-	function retry()
-	{
-		bob.playAnim('retry');
-		bosip.playAnim('idle');
-		FlxTween.tween(gameoverBlack, {alpha: 0}, 2, {
-			onComplete: (tween) ->
+			birdList.forEachAlive(function(bird)
 			{
-				FlxG.switchState(new PlayState());
+				usedTimeList.push(bird.time);
+			});
+
+			for (note in noteList)
+			{
+				if (!usedTimeList.contains(note.time) && conductor.getMil() < note.time)
+				{
+					birdList.add(new Bird(note.id, note.time, conductor.getBPM().bpm));
+				}
 			}
-		});
-	}
-
-	function changeHealth(health:Int)
-	{
-		bosip.health += health;
-		if (bosip.health < 0)
-		{
-			bosip.health = 0;
-		}
-		else if (bosip.health > 100)
-		{
-			bosip.health = 100;
 		}
 
-		healthBar.percent = bosip.health;
-
-		FlxTween.tween(healthP1, {x: healthBar.percent / 100 * 590 + 255}, 0.5, {ease: FlxEase.expoOut});
-		FlxTween.tween(healthP2, {x: healthBar.percent / 100 * 590 + 305}, 0.5, {ease: FlxEase.expoOut});
-
-		if (healthBar.percent <= 0)
+		function getRank(time:Int)
 		{
-			gameover();
-		}
-	}
+			var difference = Math.abs(conductor.getMil() - time);
 
-	override function onFocusLost()
-	{
-		if (conductor != null)
+			for (index => rank in rankings)
+			{
+				if (difference < rank)
+					return index;
+			}
+			return rankings.length;
+		}
+
+		function gameover()
+		{
+			isGameover = true;
+			bob.playAnim('gameover');
+			bosip.playAnim('gameover');
 			conductor.pause();
-		else
-			countConductor.pause();
-		super.onFocusLost();
-	}
 
-	override function onFocus()
-	{
-		if (!isGameover)
+			FlxTween.tween(songInst, {volume: 0}, 2);
+			FlxTween.tween(gameoverBlack, {alpha: 0.9}, 2);
+		}
+
+		function retry()
+		{
+			bob.playAnim('retry');
+			bosip.playAnim('idle');
+			FlxTween.tween(gameoverBlack, {alpha: 0}, 2, {
+				onComplete: (tween) ->
+				{
+					FlxG.switchState(new PlayState());
+				}
+			});
+		}
+
+		function win()
+		{
+			bob.playAnim('happy');
+			bosip.playAnim('happy');
+			var startFade = new Timer(1000);
+			startFade.run = () ->
+			{
+				openSubState(new SongFinishedSubState(rankList));
+				startFade.stop();
+			}
+		}
+
+		function changeHealth(health:Int)
+		{
+			bosip.health += health;
+			if (bosip.health < 0)
+			{
+				bosip.health = 0;
+			}
+			else if (bosip.health > 100)
+			{
+				bosip.health = 100;
+			}
+
+			healthBar.percent = bosip.health;
+
+			FlxTween.tween(healthP1, {x: healthBar.percent / 100 * 590 + 255}, 0.5, {ease: FlxEase.expoOut});
+			FlxTween.tween(healthP2, {x: healthBar.percent / 100 * 590 + 305}, 0.5, {ease: FlxEase.expoOut});
+
+			if (healthBar.percent <= 0)
+			{
+				gameover();
+			}
+		}
+
+		override function onFocusLost()
 		{
 			if (conductor != null)
-			{
-				conductor.unPause();
-				songInst.time = conductor.getMil();
-			}
+				conductor.pause();
 			else
-				countConductor.unPause();
+				countConductor.pause();
+			super.onFocusLost();
 		}
 
-		super.onFocus();
+		override function onFocus()
+		{
+			if (!isGameover)
+			{
+				if (conductor != null)
+				{
+					conductor.unPause();
+					songInst.time = conductor.getMil();
+				}
+				else
+					countConductor.unPause();
+			}
+
+			super.onFocus();
+		}
 	}
-}
-}}
